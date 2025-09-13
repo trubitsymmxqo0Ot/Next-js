2f044c44c1da84b76f0ffdd5b181d8fe32fe8068
Maxim <maxtrubitsyn3@mail.ru>
2025-09-13 19:46:17 +0300

    Немного поменял Header
    *Подогнал текст под тематику сайта
    *Разобрался что такое Image, Link, usePathname, как они работают

--- src/component/UI/Header.tsx
+++ src/component/UI/Header.tsx
@@ -1,50 +1,64 @@
 "use client";
-import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button} from "@heroui/react";
-
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Link,
+  Button,
+} from "@heroui/react";
+import Logo from "../../../public/logo_tatar_kitchen.png";
+import Image from "next/image";
+import { site } from "@/config/site.config";
+import { usePathname } from "next/navigation";
 export const AcmeLogo = () => {
   return (
-    <svg fill="none" height="36" viewBox="0 0 32 32" width="36">
-      <path
-        clipRule="evenodd"
-        d="M17.6482 10.1305L15.8785 7.02583L7.02979 22.5499H10.5278L17.6482 10.1305ZM19.8798 14.0457L18.11 17.1983L19.394 19.4511H16.8453L15.1056 22.5499H24.7272L19.8798 14.0457Z"
-        fill="currentColor"
-        fillRule="evenodd"
-      />
-    </svg>
+    <Image
+      src={Logo}
+      alt={site.title}
+      width={42}
+      height={42}
+      priority
+      className="block mr-4"
+    />
   );
 };
 
 export default function Header() {
+  const path = usePathname();
+  const headerMenu = () => {
+    return site.paths.map((item) => {
+      const isActive = path === item.path;
+      return (
+        <NavbarItem key={item.path}>
+          <Link
+            href={item.path}
+            className={`p-2 ${isActive ? "text-indigo-600" : "text-white"}`}
+          >
+            {item.name}
+          </Link>
+        </NavbarItem>
+      );
+    });
+  };
   return (
     <Navbar>
       <NavbarBrand>
+        <Link href="/">
         <AcmeLogo />
-        <p className="font-bold text-inherit">ACME</p>
+        <p className="font-bold text-inherit text-white">{site.title}</p>
+        </Link>
       </NavbarBrand>
       <NavbarContent className="hidden sm:flex gap-4" justify="center">
-        <NavbarItem>
-          <Link color="foreground" href="#">
-            Features
-          </Link>
-        </NavbarItem>
-        <NavbarItem isActive>
-          <Link aria-current="page" href="#">
-            Customers
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link color="foreground" href="#">
-            Integrations
-          </Link>
-        </NavbarItem>
+        {headerMenu()}
       </NavbarContent>
       <NavbarContent justify="end">
         <NavbarItem className="hidden lg:flex">
-          <Link href="#">Login</Link>
+          <Link href="#">Войти</Link>
         </NavbarItem>
         <NavbarItem>
           <Button as={Link} color="primary" href="#" variant="flat">
-            Sign Up
+            Зарегистрироваться
           </Button>
         </NavbarItem>
       </NavbarContent>
---
+++ src/config/site.config.ts
@@ -0,0 +1,8 @@
+export const site = { 
+	title: 'Татарская кухня',
+	paths: [
+		{name: 'Рецепты', path: '/recipes'},
+		{name: "Ингредиенты", path: '/ingerdients'},
+		{name: "О нас", path: '/about'}
+	]
+}
